7160574257a72c657d52efd42222aa318afbcc80
Ayush <aadarsha0997@gmail.com>
2024-09-17 20:41:06 +0545

    type of models

--- book_outlet/admin.py
+++ book_outlet/admin.py
@@ -1,13 +1,16 @@
 from django.contrib import admin
-from .models import Book
+from .models import Book,Author
 # Register your models here.
 
 class BookAdmin(admin.ModelAdmin):
     prepopulated_fields={"slug":("title",)}
     list_filter=("author","rating",)
     list_display=("title","author","rating",)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display=("first_name","last_name",)    
     
   
 
 
 admin.site.register(Book,BookAdmin)
+admin.site.register(Author,AuthorAdmin)
--- book_outlet/models.py
+++ book_outlet/models.py
@@ -5,14 +5,24 @@ from django.urls import reverse
 from django.utils.text import slugify
 # Create your models here.
 
+class Adress(models.Model):
+    street=models.CharField(max_length=50)
+    postal_code=models.CharField(max_length=50)
+    city=models.CharField(max_length=50)
+
 class Author(models.Model):
     first_name=models.CharField(max_length=30)
     last_name=models.CharField(max_length=30)
+    adress=models.OneToOneField(Adress, on_delete=models.CASCADE,null=True)
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+    def __str__(self):
+        return self.full_name()
 
 class Book(models.Model):
     title= models.CharField(max_length=50)
     rating= models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(5)])
-    author= models.ForeignKey(Author,on_delete=models.CASCADE,null=True)
+    author= models.ForeignKey(Author,on_delete=models.CASCADE,null=True,related_name="books")
     is_bestselling=models.BooleanField(default=False)
     slug= models.SlugField(default="",null=False,db_index=True)
     def get_absolute_url(self):
